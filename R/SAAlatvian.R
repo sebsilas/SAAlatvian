30241a4fcc2c95ea231099303cd7a42a9107786a
sebsilas <sebsilas@gmail.com>
2023-11-13 14:34:46 +0100

    SAA Latvian

--- R/SAAlatvian.R
+++ R/SAAlatvian.R
@@ -12,17 +12,17 @@
 #'
 #' @examples
 SAAlatvian <- function(app_name = "SAAlatvian",
-                       no_folk_songs = 8,
+                       no_folk_songs = 10L,
                        languages = c("lv", "en")) {
 
   language <- match.arg(languages)
 
   SAA::SAA_standalone(
     app_name = app_name,
-    num_items = list(long_tones = 0L, arrhythmic = 0L, rhythmic = 0L),
+    num_items = list(long_tones = 6L, arrhythmic = 0L, rhythmic = 10L),
     skip_setup = 'except_microphone',
     default_range = list(bottom_range = 48, top_range = 72),
-    append_trial_block_after = latvian_folk_songs_block(no_folk_songs, language),
+    append_trial_block_before = latvian_folk_songs_block(no_folk_songs, language),
     languages = "lv"
   )
 }
