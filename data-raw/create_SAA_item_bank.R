f0c0d3593774b9e9db27321bb589da71193c79d5
sebsilas <sebsilas@gmail.com>
2024-12-03 16:11:07 +0100

    Scale minimum duration to be .40

--- data-raw/create_SAA_item_bank.R
+++ data-raw/create_SAA_item_bank.R
@@ -7,11 +7,13 @@ latvian_filtered_item_bank <- SAA::Berkowitz_item_bank_subset %>%
   rowwise() %>%
   mutate(total_duration = sum(itembankr::str_mel_to_vector(durations), na.rm = TRUE),
          min_duration = min(itembankr::str_mel_to_vector(durations), na.rm = TRUE),
-         max_duration = max(itembankr::str_mel_to_vector(durations), na.rm = TRUE)
+         max_duration = max(itembankr::str_mel_to_vector(durations), na.rm = TRUE),
+         durations = paste0(if(min_duration < .40) itembankr::str_mel_to_vector(durations) * .40/min_duration else durations, collapse = ",")
   ) %>%
   ungroup() %>%
   filter(total_duration < 6,
          N < 13) %>%
+  # Scale melodies with min duration < .40 to be higher
   itembankr::set_item_bank_class()
 
 
